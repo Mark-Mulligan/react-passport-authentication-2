329602d315f2e778b6f57931df60acce9f94ff73
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-13 14:02:05 -0500

    more setup

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes");
+const cors = require('cors');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const connectDB = require("./config/db");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.static('.'));
+
+connectDB();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(cors());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view soemthing
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
